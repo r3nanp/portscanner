9f16450d4399036afd29bb3aabc91c20d65ccbcc
Renan Pereira <47953339+rghost19@users.noreply.github.com>
2019-05-08 12:46:06 -0300

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,9 @@
+import socket 
+
+portas = [21, 23, 80, 443, 8080, 3306]
+for porta in portas:
+	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	cliente.settimeout(tempo)
+	codigo = cliente.connect_ex(('http://exemplo.com', porta))
+	if codigo == 0:
+		print porta, 'OPEN'
