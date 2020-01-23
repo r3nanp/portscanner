e861856bf9f0bdba6ac609bf4dceda1411abfe1d
Renan Pereira <47953339+rghost19@users.noreply.github.com>
2020-01-22 23:27:36 -0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,10 +1,10 @@
 ''' Todos os direitos reservados a Renan Pereira de Souza '''
 import socket 
 
-portas = [21, 23, 80, 443, 8080, 3306]
-for porta in portas:
+doors = [21, 23, 80, 443, 8080, 3306]
+for door in doors:
 	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	cliente.settimeout(tempo)
-	codigo = cliente.connect_ex(('http://exemplo.com', porta))
+	cliente.settimeout(0.1)
+	codigo = cliente.connect_ex(('http://example.com', door))
 	if codigo == 0:
-		print porta, 'OPEN'
+		print door, 'OPEN'
