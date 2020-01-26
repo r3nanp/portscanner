3438380b5d1fc4722ce20e78bea82a2279103fd7
Renan Pereira <47953339+rghost19@users.noreply.github.com>
2020-01-26 12:29:15 -0300

    changing colors of CLI.

--- portscanner.py
+++ portscanner.py
@@ -9,23 +9,23 @@ N = '\033[0m'
 q = 'which ports do you wanna werify? '
 ports = list(map(int, input(q).split(' ')))
 
-
 website = input('what website you wanna verify? ')
 
 os.system('clear')
 
-print (B + "[#] The verification started on " + website + "|| Port: " + str(ports))
+print (R+ "[#] The verification started on " + website + " || Port: " + str(ports))
 
 time.sleep(2)
 
-os.system('clear')
-
+try:
 # Here is where the magic happens
-for port in ports:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.1)
-    code = client.connect_ex((website, port))
-    if code == 0:
-        print(port, 'OPEN')
-    else:
-        print('THIS PORT IS NOT OPEN')
+    for port in ports:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.1)
+        code = client.connect_ex((website, port))
+        if code == 0:
+            print(port, 'OPEN')
+        else:
+            print(port, 'THIS PORT IS NOT OPEN')
+except:
+    print('THERE IS AN ERROR')
